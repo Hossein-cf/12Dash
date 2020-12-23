4ca0315a3ae634501a745cc80eca8b64ab5c3de6
Hossein <shakryhsyn1@gmail.com>
2020-12-23 13:12:43 +0330

    add models

--- app/src/main/java/com/example/a12dash/activity_fill_information.java
+++ app/src/main/java/com/example/a12dash/activity_fill_information.java
@@ -6,7 +6,9 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class activity_fill_information extends AppCompatActivity {
@@ -16,6 +18,18 @@ public class activity_fill_information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_information);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner);
+//create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+
         Bundle b = getIntent().getExtras();
         TextView txtName = findViewById(R.id.txtxName1);
         String GameType = b.get("Game_Type").toString();
