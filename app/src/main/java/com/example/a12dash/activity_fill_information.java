da118d29f4e53198f1c5143efa709b163df4ffd5
Ghader Sahlehi <amirghsl1380@gmail.com>
2020-12-24 20:45:14 -0800

    ...

--- app/src/main/java/com/example/a12dash/activity_fill_information.java
+++ app/src/main/java/com/example/a12dash/activity_fill_information.java
@@ -3,11 +3,9 @@ package com.example.a12dash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class activity_fill_information extends AppCompatActivity {
 
@@ -16,33 +14,38 @@ public class activity_fill_information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_information);
+
+        String[] Colors = new String[]{"Pick a Color","Blue", "Red", "Green"};
+        String[] Players = new String[]{"Pick a player","First one", "Second one"};
+
+        Spinner firstPlayerColorDropdown = findViewById(R.id.firstPlayerColor);
+        ArrayAdapter<String> firstAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, Colors);
+        firstPlayerColorDropdown.setAdapter(firstAdapter);
+
+
+        Spinner secondPlayerColorDropdown = findViewById(R.id.secondPlayerColor);
+        ArrayAdapter<String> secondAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, Colors);
+        secondPlayerColorDropdown.setAdapter(secondAdapter);
+
+
+        Spinner playersDropdown = findViewById(R.id.playersDropdown);
+        ArrayAdapter<String> playersAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, Players);
+        playersDropdown.setAdapter(playersAdapter);
+//        Bundle b = getIntent().getExtras();
+//        TextView txtName = findViewById(R.id.txtxNameFirst);
+//        String GameType = b.get("Game_Type").toString();
+//        System.out.println(GameType);
+//        if (GameType.equals("SinglePlayer")) {
+//            txtName.setText("system");
 //
-//        //get the spinner from the xml.
-//        Spinner dropdown = findViewById(R.id.spinner);
-////create a list of items for the spinner.
-//        String[] items = new String[]{"1", "2", "three"};
-////create an adapter to describe how the items are displayed, adapters are used in several places in android.
-////There are multiple variations of this, but this is the basic variant.
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-////set the spinners adapter to the previously created one.
-//        dropdown.setAdapter(adapter);
-
-
-        Bundle b = getIntent().getExtras();
-        TextView txtName = findViewById(R.id.txtxNameFirst);
-        String GameType = b.get("Game_Type").toString();
-        System.out.println(GameType);
-        if (GameType.equals("SinglePlayer")) {
-            txtName.setText("system");
-
-        }
-        Button btnStart = findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Game_Page_Activity.class);
-                startActivity(intent);
-            }
-        });
+//        }
+//        Button btnStart = findViewById(R.id.btnStart);
+//        btnStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), Game_Page_Activity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
